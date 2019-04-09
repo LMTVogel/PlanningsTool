7a34ba1522c971e00efea2c6de81119930a9df11
Luuk Vogel <99051050@mydavinci.nl>
2019-04-09 14:25:34 +0200

    added the images

--- connect.php
+++ connect.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	
-</body>
-</html>
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "mysql";
+	$myDB = "planningsTool";
+
+	header('Content-type: text/html; charset=iso-8859-1');
+
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "WE ARE IN THE AIR";
+	} catch(PDOException $e) {
+		echo "NIET GELUKT" . $e->getMessage();
+	} 
+?>
